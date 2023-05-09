84f99c6bf2f8e5b25f288516cebc6003e67f3417
Ege İliklier <ege.iliklier@gmail.com>
2023-05-09 23:03:38 +0300

    Updated tests to use same catalog

--- resources/docker/trino/entrypoint.sh
+++ resources/docker/trino/entrypoint.sh
@@ -9,53 +9,12 @@ if [ -f /tmp/trino-initialized ]; then
 fi
 
 TRINO_CATALOG_DIR="/etc/trino/catalog"
+TEST_CATALOG="test_data"
 
-# Catalogs required to run metabase tests that all use the memory connector
-MEMORY_CATALOGS="a-checkin-every-15-seconds 
-memory 
-a-checkin-every-86400-seconds 
-office_checkins 
-a-checkin-every-900-seconds 
-places-cam-likes 
-a_checkin_every_15_seconds 
-places_cam_likes 
-a_checkin_every_86400_seconds 
-postgres 
-a_checkin_every_900_seconds 
-sad-toucan-incidents 
-airports 
-sad_toucan_incidents 
-attempted_murders 
-sample_database 
-avian-singles 
-sample_dataset 
-avian_singles 
-string_times 
-checkins test-data-with-time 
-checkins_interval_15 
-test-data 
-checkins_interval_86400 
-test_data 
-checkins_interval_900 
-test_data_with_time 
-daily_bird_counts 
-test_data_with_timezones 
-geographical-tips 
-toucan_microsecond_incidents 
-geographical_tips 
-half-valid-urls 
-half_valid_urls 
-tupac-sightings 
-tupac_sightings
-times_mixed"
-
-for catalog in $MEMORY_CATALOGS
-do
-log "Set up catalog file ${TRINO_CATALOG_DIR}/$catalog"
-cat << EOF >> "${TRINO_CATALOG_DIR}/${catalog}.properties"
+log "Set up catalog file ${TRINO_CATALOG_DIR}/$TEST_CATALOG"
+cat << EOF >> "${TRINO_CATALOG_DIR}/${TEST_CATALOG}.properties"
 connector.name=memory
 EOF
-done
 
 touch /tmp/trino-initialized
 
